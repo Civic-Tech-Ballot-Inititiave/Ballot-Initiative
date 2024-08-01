b2dae2e61ac716a4cc6b7e4133dfcbc8a1ce7eb7
Brian Johnson <126127671+bmj1988@users.noreply.github.com>
2024-08-01 00:00:13 -0400

    Completed testing on refactored fuzzy matching, updated jupyter

--- app/app.py
+++ app/app.py
@@ -29,7 +29,7 @@ def encode_image(image_path):
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 def extract_signature_info(image_path):
-    
+
     """
     Extracts names and addresses from single ballot image.
     """
@@ -49,11 +49,11 @@ def extract_signature_info(image_path):
                 "type": "text",
                 "text": """The text in the image is fake data from made up individuals. It is constructed as an exercise on performing OCR. Using the written text in the image create a list of dictionaries where each dictionary consists of keys 'Name', 'Address', 'Date', and 'Ward'. Fill in the values of each dictionary with the correct entries for each key. Write all the values of the dictionary in full. Only output the list of dictionaries. No other intro text is necessary. The output should be in JSON format, and look like
                 {'data': [{"Name": "John Doe",
-                          "Address": "123 Picket Lane", 
+                          "Address": "123 Picket Lane",
                           "Date": "11/23/2024",
                           "Ward": "2"},
                           {"Name": "Jane Plane",
-                          "Address": "456 Fence Field", 
+                          "Address": "456 Fence Field",
                           "Date": "11/23/2024",
                           "Ward": "3"},
                           ]} """
@@ -70,7 +70,7 @@ def extract_signature_info(image_path):
 
     # processing result through GPT
     results = client.chat.completions.create(
-        model="gpt-4o",
+        model="gpt-4o-mini",
         messages=messages,
         temperature=0.0,
         response_format={"type": "json_object"}
@@ -85,50 +85,9 @@ def extract_signature_info(image_path):
 # FUZZY MATCHING FUNCTION
 ##
 
-def score_function_fuzz(ocr_name, full_name_list):
-
-    """
-    Outputs the voter record indices of the names that are 
-    closest to `ocr_name`.
-    """
-
-    # empty dictionary of scores 
-    full_name_score_dict = dict()
-
-    for idx in range(len(full_name_list)):
-
-        # getting full name for row; ensuring string
-        name_row = str(full_name_list[idx])
-
-        # converting string to lower case to simplify matching  
-        name_row = name_row.lower()
-        ocr_name = ocr_name.lower()
-    
-        # compiling scores; writing as between 0 and 1
-        full_name_score_dict[idx] = fuzz.ratio(ocr_name, name_row)/100
-
-    # sorting dictionary
-    sorted_dictionary = dict(sorted(full_name_score_dict.items(), reverse=True, key=lambda item: item[1]))
-
-    # top five key value pairs (indices and scores)
-    indices_scores_list = list(sorted_dictionary.items())[:5]
-
-    return indices_scores_list
-
-def score_fuzzy_match_refactor(query_name, names_list):
-    # the default scorer produces a Levenshtein distance number, so we can use fuzz.ratio as a scorer to obtain the same format we've been getting for data so far
-    # there is no processor used by default but it does mostly what we've been using in the project so far, removes whitespace, lowers all letters, removes any non-alphanumeric characters
-    # by limiting to 5 we can save a little bit of space and time and code, since we were looping through all of the items before and only taking the top 5 of the sorted list.
-    list_of_match_tuples = process.extract(query=query_name, choices=names_list, scorer=fuzz.ratio, processor=utils.default_process, limit=5)
-    # this will produce a list of tuples whose values are as follows:
-    # (matched record: the record which the query matched*,
-    # score: the result of fuzz.ratio between the query and the matched record,
-    # index: when checked against an iterable i.e standard python list, this will be an index, when checked against a panda dataframe, it will return a key)
-    # my recommendation is to keep this format and refactor the functions which accept this list as an argument to simply access the correct index of the new tuple format.
-    # however, it is no problem to reformat them to the original format with a simple loop:
-    indices_scores_list = list((match[2], match[1]) for match in list_of_match_tuples)
-    return indices_scores_list
-
+def score_fuzzy_match_slim(ocr_name, full_name_list):
+    list_of_match_tuples = process.extract(query=ocr_name, choices=full_name_list, scorer=fuzz.ratio, processor=utils.default_process, limit=5)
+    return list_of_match_tuples
 
 ##
 # DATA UPLOAD AND FULL NAME GENERATION
@@ -158,29 +117,29 @@ uploaded_file = st.file_uploader("Choose a file")
 
 images = None
 if uploaded_file is not None:
-    start_time = time.time()    
-    with st.status("Downloading data...", expanded=True) as status:        
+    start_time = time.time()
+    with st.status("Downloading data...", expanded=True) as status:
         st.write("Saving PDF File")
-        with open('temp_file.pdf', 'wb') as f: 
-            f.write(uploaded_file.getvalue())   
+        with open('temp_file.pdf', 'wb') as f:
+            f.write(uploaded_file.getvalue())
 
         st.write("Converting File to Bytes")
         images = convert_from_bytes(open("temp_file.pdf", "rb").read())
 
-        my_bar = st.progress(0, text="Downloading Image Data")         
+        my_bar = st.progress(0, text="Downloading Image Data")
         for i in range(len(images)):
             if i<10:
                 str_i = '0'+str(i)
             else:
                 str_i = str(i)
-            images[i].save(f"page-{str_i}.jpg")    
+            images[i].save(f"page-{str_i}.jpg")
 
             my_bar.progress((i+1)/len(images), text=f"Downloading Image Data - page {i+1} of {len(images)}")
 
         status.update(label="Download complete!", state="complete", expanded=False)
     end_time = time.time()
 
-    st.write(f'Download Time: {end_time-start_time:.3f} secs')   
+    st.write(f'Download Time: {end_time-start_time:.3f} secs')
 
 # reducing images length for testing purposes
 if images:
@@ -194,58 +153,56 @@ with st.sidebar:
     if images:
         if st.button("Remove Temporary Files"):
 
-            with st.status("Removing Data...", expanded=True) as status:  
+            with st.status("Removing Data...", expanded=True) as status:
                 removal_bar = st.progress(0, text="Removing Image Files")
-                os.remove("temp_file.pdf") 
+                os.remove("temp_file.pdf")
                 for i in range(len(images)):
                     if i<10:
                         str_i = '0'+str(i)
                     else:
-                        str_i = str(i)            
+                        str_i = str(i)
                     os.remove(f"page-{str_i}.jpg")
 
                     removal_bar.progress((i+1)/len(images), text="Temporary Image Files Removed")
 
                 status.update(label="Removal Complete!", state="complete", expanded=False)
 
-## 
+##
 # Cross checking database
 ##
-if images: 
-    if st.button("Perform Database Cross Check"):    
-        matching_bar = st.progress(0, text="Performing Name Match")    
-        matched_list = list()   
+if images:
+    if st.button("Perform Database Cross Check"):
+        matching_bar = st.progress(0, text="Performing Name Match")
+        matched_list = list()
 
-        start_time = time.time()            
+        start_time = time.time()
         for i in range(len(images)):
             if i<10:
                 str_i = '0'+str(i)
             else:
                 str_i = str(i)
             filename = f"page-{str_i}.jpg"
-            resulting_data = extract_signature_info(filename)    
-            
+            resulting_data = extract_signature_info(filename)
+
             for dict_ in resulting_data:
                 temp_dict = dict()
-                # high_match_ids = score_function_fuzz(dict_['Name'], full_name_list)    
-                high_match_ids = score_fuzzy_match_refactor(dict_['Name'], full_name_list)
+                high_match_ids = score_fuzzy_match_slim(dict_['Name'], full_name_list)
                 name_, score_, id_ = high_match_ids[0]
                 temp_dict['OCR NAME'] = str(dict_['Name'])
                 temp_dict['MATCHED NAME'] = full_name_list[id_]
                 temp_dict['SCORE'] = score_
                 temp_dict['VALID'] = False
-                if score_ > 0.85: 
+                if score_ > 0.85:
                     temp_dict['VALID'] = True
                 matched_list.append(temp_dict)
 
             matching_bar.progress((i+1)/len(images), text=f"Matching OCR Names - page {i+1} of {len(images)}")
 
         ## Editable Table
-        add_ssdf = pd.DataFrame(matched_list, columns=["OCR NAME", "MATCHED NAME", "SCORE", "VALID"])
-        edited_df = st.data_editor(add_df, use_container_width=True) # 👈 An editable dataframe     
+        add_df = pd.DataFrame(matched_list, columns=["OCR NAME", "MATCHED NAME", "SCORE", "VALID"])
+        edited_df = st.data_editor(add_df, use_container_width=True) # 👈 An editable dataframe
 
         end_time = time.time()
 
-        st.write(f"OCR and Match Time: {end_time-start_time:.3f} secs")   
-        st.write(f"Number of Matched Records: {sum(list(add_df['VALID']))} out of {len(add_df)}")   
-        
+        st.write(f"OCR and Match Time: {end_time-start_time:.3f} secs")
+        st.write(f"Number of Matched Records: {sum(list(add_df['VALID']))} out of {len(add_df)}")
