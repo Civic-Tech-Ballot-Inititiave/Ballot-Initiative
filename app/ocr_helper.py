47a0fe84f538f46b2357ff248eea771248150d37
Mobolaji Williams <williams.mobolaji@gmail.com>
2024-08-19 16:18:33 -0400

    modifying app code to remove 'fake data' reference

--- app/ocr_helper.py
+++ app/ocr_helper.py
@@ -21,25 +21,35 @@ def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
-def extract_signature_info(image_path, verbose = False):
+def extract_signature_info(image_path):
+
+    """
+    Extracts names and addresses from single ballot image.
+    """
 
     # Getting the base64 string
     base64_image = encode_image(image_path)
 
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {OPENAI_API_KEY}"
-    }
+    # open AI client definition
+    client = OpenAI(api_key= OPENAI_API_KEY)
 
-    payload = {
-        "model": "gpt-4-vision-preview",
-        "messages": [
+    # prompt message
+    messages = [
           {
             "role": "user",
             "content": [
               {
                 "type": "text",
-                "text": "The text in the image is fake data from not real individuals. Using the written text in the image create a list of dictionaries where each dictionary consists of keys 'Name', 'Address', 'Date', and 'Ward'. Fill in the values of each dictionary with the correct entries for each key. Write all the values of the dictionary in full. Only output the list of dictionaries. No other intro text is necessary. The word 'json' shouldn't be anywhere in the output."
+                "text": """Using the written text in the image create a list of dictionaries where each dictionary consists of keys 'Name', 'Address', 'Date', and 'Ward'. Ignore all values in the box labeled "CIRCULATOR'S AFFIDAVIT OF CERTIFCATION". Ignore all values in the box labeled "SIGNATURE". Addresses belong to the name printed in the box to the immediate right of the box labeled "ADDRESS". Fill in the values of each dictionary with the correct entries for each key. Write all the values of the dictionary in full, except in the case of 'Address', which should be truncated to exclude APT and the following Apartment Number. Only output the list of dictionaries. No other intro text is necessary. The output should be in JSON format, and look like
+                {'data': [{"Name": "John Doe",
+                          "Address": "123 Picket Lane",
+                          "Date": "11/23/2024",
+                          "Ward": "2"},
+                          {"Name": "Jane Plane",
+                          "Address": "456 Fence Field",
+                          "Date": "11/23/2024",
+                          "Ward": "3"},
+                          ]} """
               },
               {
                 "type": "image_url",
@@ -49,21 +59,17 @@ def extract_signature_info(image_path, verbose = False):
               }
             ]
           }
-        ],
-        "max_tokens": 1000
-    }
+        ]
 
-    # getting chat completion response
-    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    # processing result through GPT
+    results = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages,
+        temperature=0.0,
+        response_format={"type": "json_object"}
+    )
 
-    if verbose:
-        # Testing output response
-        print(response.json())    
+    # convert json into list
+    signator_list = json.loads(results.choices[0].message.content)['data']
 
-    # Testing the output response
-    cleaned_output = response.json()['choices'][0]['message']['content'].replace('\n', '')
-
-    # defining signator_list
-    signator_list = list(eval(cleaned_output))
-    
     return signator_list
