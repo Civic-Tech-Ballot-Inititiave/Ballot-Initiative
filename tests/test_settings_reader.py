c7ae8884693b3e500b4cca525b6a1e21466df212
Kurian Vithayathil <noreply@git.com>
2025-03-08 22:43:21 -0500

    Add tests for gemini config

--- tests/test_settings_reader.py
+++ tests/test_settings_reader.py
@@ -19,6 +19,13 @@ def test_load_ocr_config_for_mistral():
     assert config.api_key == "Your Mistral API key"
     assert config.model == "default"
 
+def test_load_ocr_config_for_gemini():
+    test_settings["selected_ocr_engine"] = "gemini_ai"
+    config = load_ocr_config("gemini_ai", test_settings) 
+    assert isinstance(config, GeminiAIConfig)
+    assert config.api_key == "Your Gemini API key"
+    assert config.model == "default"
+
 def test_load_ocr_raise_error_with_invalid_ai_selected():
     test_settings["selected_ocr_engine"] = "invalid_ai"
     with pytest.raises(KeyError) as exc_info:
@@ -33,5 +40,3 @@ def test_load_ocr_raise_error_with_misconfigured_config():
         load_ocr_config("invalid_ai", test_settings)
 
     assert "Could not find configuration for invalid_ai. Please check your settings file." in str(exc_info.value)
-
-
